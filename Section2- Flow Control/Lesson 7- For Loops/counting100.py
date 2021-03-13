85f771aad23e74e7e47d7e9dabbf8f0b7d06a1b6
Spencer Truett <srtruett1993@gmail.com>
2021-03-13 16:16:10 -0600

    added for loop example

---
+++ Section2- Flow Control/Lesson 7- For Loops/counting100.py
@@ -0,0 +1,6 @@
+#Based on Carl Gauss
+
+total = 0
+for num in range(101): #ints 0 to 100
+    total = total + num
+print(total)
---
+++ Section2- Flow Control/Lesson 7- For Loops/fiveTimesRange.py
@@ -0,0 +1,3 @@
+print('My name is')
+for i in range(8, 16, 2): #first number is where it starts, second is where it ends
+    print('Jimmy Five Time ' +str(i))
